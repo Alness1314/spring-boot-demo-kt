9fdb6759e3744818f7bce1e5c8b77df656b5d6fd
Alberto Ordaz <albert0rz1213@gmail.com>
2025-09-25 01:47:42 -0600

    feat: :zap: entity modules

---
+++ src/main/kotlin/com/example/demokot/modules/entity/ModulesEntity.kt
@@ -0,0 +1,57 @@
+package com.example.demokot.modules.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.PrePersist
+import jakarta.persistence.PreUpdate
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+import java.util.UUID
+
+@Entity
+@Table(name = "modules")
+open class ModulesEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    var id: UUID,
+
+    @Column(name = "title", nullable = false, unique = true, length = 128)
+    var title: String,
+
+    @Column(name = "subTitle", nullable = true, length = 1024)
+    var subTitle: String,
+
+    @Column(name = "path", nullable = false, length = 512)
+    var path: String,
+
+    @Column(name = "icon", nullable = true, length = 256)
+    var icon: String,
+
+    @Column(name = "level", nullable = false, length = 128)
+    var level: String,
+
+    @Column(name = "created_at", updatable = false)
+    var createdAt: LocalDateTime? = null,
+
+    @Column(name = "updated_at")
+    var updatedAt: LocalDateTime? = null,
+
+    @Column(name = "erased", nullable = false)
+    var erased: Boolean = false
+){
+    @PrePersist
+    fun prePersist() {
+        erased = false
+        createdAt = LocalDateTime.now()
+        updatedAt = LocalDateTime.now()
+    }
+
+    @PreUpdate
+    fun preUpdate() {
+        updatedAt = LocalDateTime.now()
+    }
+}
